b404137d633a292c0af70cb4964136c85c19cec5
Caden Miller <cullvox@outlook.com>
2026-01-25 16:12:16 -0500

    Working on adding properties

--- Source/Engine/Scene/Node.h
+++ Source/Engine/Scene/Node.h
@@ -3,12 +3,46 @@
 #include <stdexcept>
 #include <memory>
 #include <string>
+#include <string_view>
 #include <unordered_map>
+
+#include <Core/Variant.h>
 
 namespace bl {
 
 class Engine;
 class RenderData;
+
+class PropertyBase {
+public:
+    virtual void Set(Node* node, const Variant& value) = 0;
+    virtual Variant Get(Node* node) = 0;
+};
+
+template<typename N, typename T>
+class Property : public PropertyBase {
+    T N::* _ptr;
+    std::string_view _name;
+
+public:
+    Property(std::string_view name, N*::T value)
+        : _name(name)
+        , _ptr(value)
+    {
+    }
+
+    virtual void Set(Node* node, const Variant& value)
+    {
+        if (auto n = dynamic_cast<N>(node))
+            n->*_ptr = value;
+    }
+
+    virtual Variant Get(Node* node)
+    {
+        if (auto n = dynamic_cast<N>(node))
+            n->*_ptr = value;
+    }
+};
 
 class Node {
     std::string _name;
@@ -16,6 +50,8 @@ class Node {
     std::vector<std::unique_ptr<Node>> _children;
     std::unordered_map<std::string, Node*> _childrenMap;
     Engine& _engine;
+    static std::vector<PropertyBase> _properties;
+    static std::unordered_map<std::string_view, int> _propertyNameToIndex;
 
 protected:
     friend class NodeFilterIterator;
@@ -42,6 +78,11 @@ public:
             throw std::runtime_error("Failed to cast Node to desired type!");
         }
     }
+
+    void Set(const std::string& name, const Variant& value);
+    Variant Get(const std::string& name);
+    void Set(int propertyIndex, const Variant& value);
+    Variant Get(int propertyIndex);
 
     Engine& GetEngine();
     bool SetName(const std::string& name);
